608960cf70a3aca2a6efc418adeaffaea83623da
Connect\Alejandro <alejo.go4@hotmail.com>
2017-03-31 14:06:17 -0500

    Incluir login

--- assets/js/scriptsAngular.js
+++ assets/js/scriptsAngular.js
@@ -2,17 +2,41 @@
 var myApp = angular.module('apptest', ['ui.router']);
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
+        
             $stateProvider
                 .state('pages', {
                     url: '/pages',
-                    templateUrl: 'views/pages.html'
+                    templateUrl: 'views/pages.html',
+                    controller: 'pageHome'
       
                 })
                 .state('photos', {
                     url: '/photos',
-                    templateUrl: 'views/photos.html'
+                    templateUrl: 'views/photos.html',
+                    controller: 'pagePhoto'
+                })
+                .state('login', {
+                    url: '/login',
+                    templateUrl: 'views/login.html',
+                    controller: 'pageLogin'
                 });
 
             $urlRouterProvider.otherwise('pages'); //Default para cargar esta pagina por defecto
     
     })
+
+myApp.controller('pageHome',function($scope,$state){
+    $scope.icons = true;
+    
+})
+
+
+myApp.controller('pagePhoto',function($scope,$state){
+    
+    
+})
+
+myApp.controller('pageLogin',function($scope,$state){
+    
+
+})
